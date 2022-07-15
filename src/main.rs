22cd80e740c8bc8e17d01b8422b86379575b6204
Juliette Cordor <grassojames5@gmail.com>
2022-07-15 15:03:59 +1000

    fix

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 #![feature(asm_const)]
+#![feature(format_args_nl)]
 #![no_main]
 #![no_std]
 
@@ -14,5 +15,7 @@ const _: () = if !cfg!(target_arch = "aarch64") {
 };
 
 unsafe fn kernel_init() -> ! {
+    println!("kernel_init() called");
+
     panic!()
 }
