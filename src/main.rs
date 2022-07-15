0f92abec062526fe36264bf66c489f56b5fc2116
Juliette Cordor <grassojames5@gmail.com>
2022-07-15 21:21:34 +1000

    removed allocation as currently not working

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,14 @@
 #![no_main]
 #![no_std]
 
-mod alloc;
+// mod alloc;
+// mod colorize;
+
 mod bsp;
-mod colorize;
 mod console;
 mod cpu;
 mod panic_wait;
 mod print;
-
-use colorize::Colorize;
-
-use crate::{alloc::init_alloc, colorize::Colors};
 
 // Panic if not building for aarch64
 const _: () = if !cfg!(target_arch = "aarch64") {
@@ -24,15 +21,13 @@ const _: () = if !cfg!(target_arch = "aarch64") {
 };
 
 unsafe fn kernel_init() -> ! {
-    init_alloc();
-
     use console::console;
 
     println!("[0] Hello from Rust!");
 
     println!("[1] Chars written: {}", console().chars_written());
 
-    println!("[2] Stopping here. {}", "yo".colorize(Colors::Red));
+    println!("[2] Stopping here.");
 
     cpu::wait_forever()
 }
