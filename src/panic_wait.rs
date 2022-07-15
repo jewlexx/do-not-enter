5677ca3b788e798ba1852ca8fd10c79a7545b089
Juliette Cordor <grassojames5@gmail.com>
2022-07-15 20:03:25 +1000

    fix

--- src/panic_wait.rs
+++ src/panic_wait.rs
@@ -1,7 +1,3 @@
-// SPDX-License-Identifier: MIT OR Apache-2.0
-//
-// Copyright (c) 2018-2022 Andre Richter <andre.o.richter@gmail.com>
-
 //! A panic handler that infinitely waits.
 
 use crate::{cpu, println};
