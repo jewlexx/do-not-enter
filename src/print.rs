f4990f255a0c066ae0e8fb2beaf6eb971a951dd7
Juliette Cordor <grassojames5@gmail.com>
2022-07-15 15:01:31 +1000

    added docs

--- src/print.rs
+++ src/print.rs
@@ -8,11 +8,13 @@ pub fn _print(args: fmt::Arguments) {
     console::console().write_fmt(args).unwrap();
 }
 
+/// Prints to stdout
 #[macro_export]
 macro_rules! print {
     ($($arg:tt)*) => ($crate::print::_print(format_args!($($arg)*)));
 }
 
+/// Prints to stdout with a newline
 #[macro_export]
 macro_rules! println {
     () => ($crate::print!("\n"));
