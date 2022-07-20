3a3eb4a1cb58dd34db3bfced4b64055fef49cb81
Juliette Cordor <grassojames5@gmail.com>
2022-07-20 13:10:41 +1000

    added mailbox devices to mmio

--- src/bsp/raspberrypi/memory.rs
+++ src/bsp/raspberrypi/memory.rs
@@ -13,6 +13,17 @@ pub(super) mod map {
         pub const START:            usize =         0x3F00_0000;
         pub const GPIO_START:       usize = START + GPIO_OFFSET;
         pub const PL011_UART_START: usize = START + UART_OFFSET;
+
+        pub const VIDEOCORE_MBOX:       usize = START + 0x0000B880;
+        pub const MBOX_READ:            usize = VIDEOCORE_MBOX;
+        pub const MBOX_POLL:            usize = VIDEOCORE_MBOX + 0x10;
+        pub const MBOX_SENDER:          usize = VIDEOCORE_MBOX + 0x14;
+        pub const MBOX_STATUS:          usize = VIDEOCORE_MBOX + 0x18;
+        pub const MBOX_CONFIG:          usize = VIDEOCORE_MBOX + 0x1C;
+        pub const MBOX_WRITE:           usize = VIDEOCORE_MBOX + 0x20;
+        pub const MBOX_RESPONSE:        usize = 0x80000000;
+        pub const MBOX_FULL:            usize = 0x80000000;
+        pub const MBOX_EMPTY:            usize = 0x40000000;
     }
 
     /// Physical devices.
@@ -23,5 +34,16 @@ pub(super) mod map {
         pub const START:            usize =         0xFE00_0000;
         pub const GPIO_START:       usize = START + GPIO_OFFSET;
         pub const PL011_UART_START: usize = START + UART_OFFSET;
+
+        pub const VIDEOCORE_MBOX:       usize = START + 0x0000B880;
+        pub const MBOX_READ:            usize = VIDEOCORE_MBOX;
+        pub const MBOX_POLL:            usize = VIDEOCORE_MBOX + 0x10;
+        pub const MBOX_SENDER:          usize = VIDEOCORE_MBOX + 0x14;
+        pub const MBOX_STATUS:          usize = VIDEOCORE_MBOX + 0x18;
+        pub const MBOX_CONFIG:          usize = VIDEOCORE_MBOX + 0x1C;
+        pub const MBOX_WRITE:           usize = VIDEOCORE_MBOX + 0x20;
+        pub const MBOX_RESPONSE:        usize = 0x80000000;
+        pub const MBOX_FULL:            usize = 0x80000000;
+        pub const MBOX_EMPTY:            usize = 0x40000000;
     }
 }
--- src/mail.rs
+++ src/mail.rs
@@ -13,15 +13,3 @@ unsafe fn mmio_write(src: usize, dest: *mut usize) {
 unsafe fn mbox_call(val: char) {
     while (mmio_read(MBOX_STATUS) & MBOX_FULL) {}
 }
-
-const PERIPHERAL_BASE: usize = 0xFE000000;
-const VIDEOCORE_MBOX: usize = PERIPHERAL_BASE + 0x0000B880;
-const MBOX_READ: usize = VIDEOCORE_MBOX;
-const MBOX_POLL: usize = VIDEOCORE_MBOX + 0x10;
-const MBOX_SENDER: usize = VIDEOCORE_MBOX + 0x14;
-const MBOX_STATUS: usize = VIDEOCORE_MBOX + 0x18;
-const MBOX_CONFIG: usize = VIDEOCORE_MBOX + 0x1C;
-const MBOX_WRITE: usize = VIDEOCORE_MBOX + 0x20;
-const MBOX_RESPONSE: usize = 0x80000000;
-const MBOX_FULL: usize = 0x80000000;
-const MBOX_EMPTY: usize = 0x40000000;
