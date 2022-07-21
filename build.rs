f7f616ef4e5ca2a244927f4d5b2f85de1598821c
Juliette Cordor <grassojames5@gmail.com>
2022-07-21 20:37:43 +1000

    removed compile errors from build.rs

--- build.rs
+++ build.rs
@@ -1,11 +1,6 @@
 use std::{env, fs, process};
 
 fn main() {
-    if cfg!(not(any(feature = "bsp_rpi4", feature = "bsp_rpi3"))) {
-        eprintln!("This crate is only meant to be used on Raspberry Pi. You must enable either rpi4 or rpi3!");
-        process::exit(1);
-    }
-
     let ld_script_path = match env::var("LD_SCRIPT_PATH") {
         Ok(var) => var,
         _ => process::exit(0),
