525e13dc37693f4ee102d66cb369ee01ce7cbf0a
Juliette Cordor <grassojames5@gmail.com>
2022-07-25 11:54:30 +1000

    fix

--- src/bsp/raspberrypi/memory.rs
+++ src/bsp/raspberrypi/memory.rs
@@ -19,8 +19,6 @@ pub mod map {
     pub mod mmio {
         use super::*;
 
-        pub use super::{END_INCLUSIVE, START};
-
         pub const GPIO_START: usize = START + GPIO_OFFSET;
         pub const PL011_UART_START: usize = START + UART_OFFSET;
 
