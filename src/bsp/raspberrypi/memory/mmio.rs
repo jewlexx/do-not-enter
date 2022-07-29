98a55fd99aecda76bfa896ec4ac926a95241c4a7
Juliette Cordor <grassojames5@gmail.com>
2022-07-29 14:42:30 +1000

    added docs

--- src/bsp/raspberrypi/memory/mmio.rs
+++ src/bsp/raspberrypi/memory/mmio.rs
@@ -1,11 +1,16 @@
+//! BSP MMIO
+
+/// The start of the mmio
 pub const MMIO_START: usize = 0x3F000000;
 
+/// Write to a point in mmio
 pub fn write_at_offset(data: u32, offset: usize) {
     unsafe {
         core::ptr::write_volatile((MMIO_START + offset) as *mut u32, data);
     }
 }
 
+/// Read from a point in mmio
 pub fn read_at_offset(offset: usize) -> u32 {
     unsafe { core::ptr::read_volatile((MMIO_START + offset) as *const u32) }
 }
--- src/memory/mmio.rs
+++ src/memory/mmio.rs
@@ -1,1 +1,3 @@
+//! Fixed MMIO
+
 pub use crate::bsp::memory::mmio::*;
