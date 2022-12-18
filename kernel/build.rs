15e6f611866380733dace437658f4a21ef45b215
Juliette Cordor <grassojames5@gmail.com>
2022-12-19 00:10:15 +1100

    added test c files

--- kernel/build.rs
+++ kernel/build.rs
@@ -1,6 +1,16 @@
 use std::{env, fs, process};
 
 fn main() {
+    let c_files: Vec<_> = fs::read_dir("include")
+        .unwrap()
+        .filter_map(|x| match x {
+            Ok(v) => Some(v.path()),
+            Err(_) => None,
+        })
+        .collect();
+
+    cc::Build::new().files(c_files).compile("c_bindings");
+
     let ld_script_path = match env::var("LD_SCRIPT_PATH") {
         Ok(var) => var,
         _ => process::exit(0),
---
+++ kernel/include/impl.h
@@ -0,0 +1,3 @@
+inline int returns_zero(void) { return 0; }
+
+inline int returns_one(void) { return 1; }
