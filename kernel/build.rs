eb2cb9ef247eb59356fb3de362c6e5faf8df0313
Juliette Cordor <grassojames5@gmail.com>
2022-12-19 00:22:28 +1100

    fix

--- kernel/build.rs
+++ kernel/build.rs
@@ -1,15 +1,17 @@
 use std::{env, fs, process};
 
 fn main() {
-    let c_files: Vec<_> = fs::read_dir("include")
-        .unwrap()
-        .filter_map(|x| match x {
-            Ok(v) => Some(v.path()),
-            Err(_) => None,
-        })
-        .collect();
+    // let c_files: Vec<_> = fs::read_dir("include")
+    //     .unwrap()
+    //     .filter_map(|x| match x {
+    //         Ok(v) => Some(v.path()),
+    //         Err(_) => None,
+    //     })
+    //     .collect();
 
-    cc::Build::new().files(c_files).compile("c_bindings");
+    cc::Build::new()
+        .file("include/bindings.h")
+        .compile("c_bindings");
 
     let ld_script_path = match env::var("LD_SCRIPT_PATH") {
         Ok(var) => var,
